dd4079c6f2595ab231368eaebf2af75565150111
Lee Miller <iarkeus@gmail.com>
2013-09-14 17:20:33 -0700

    Adding initialization search.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Notepadxiv::Application.routes.draw do
 	root "notepad#index"
+	get "s/:search" => "notepad#index"
+	
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
