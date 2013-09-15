48f293fe2016f8d712fb9e61e9c32e2692bd1bc1
Lee Miller <iarkeus@gmail.com>
2013-09-14 18:02:56 -0700

    Fixing em tag.

--- app/assets/javascripts/notepad.js
+++ app/assets/javascripts/notepad.js
@@ -40,7 +40,7 @@ app.directive("itemResult", ["Notepad", function(Notepad) {
 		controller: ["$scope", "$element", "$attrs", function($scope, $element, $attrs) {
 			
 		}],
-		template: "<div class='item-result tooltip' data-xivdb='http://xivdb.com/{{item.u}}'><div class='name rarity-{{item.r}}'>{{item.n}}</div><div class='info'><a class='link' href='http://xivdb.com/{{item.u}}' target='_new'>XIVDB</a><span class='level'>Lv<em>{{item.l}}<em></span> | <span class='type'>{{item.t}}</span></div></div>",
+		template: "<div class='item-result tooltip' data-xivdb='http://xivdb.com/{{item.u}}'><div class='name rarity-{{item.r}}'>{{item.n}}</div><div class='info'><a class='link' href='http://xivdb.com/{{item.u}}' target='_new'>XIVDB</a><span class='level'>Lv<em>{{item.l}}</em></span> | <span class='type'>{{item.t}}</span></div></div>",
 		replace: true,
 		link: function(scope, element, attrs) {
 			
