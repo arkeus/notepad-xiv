f8ce319dc20c62c34e558d8109de48245f162213
Lee Miller <iarkeus@gmail.com>
2013-09-21 13:09:58 -0700

    List of gathering nodes. Todo: Add missing ones.

---
+++ app/assets/javascripts/data/gathering_nodes.js
@@ -0,0 +1,25 @@
+var gatheringNodes = {
+	mining: [
+		{ type: "m", time: "1:00", item: "Darksteel Ore", zone: "Coerthas Central Highlands", area: "Haldrath's March" },
+		{ type: "q", time: "5:00", item: "Gold Sand", zone: "Eastern Thanalan", area: "Yug'ram River" },
+		{ type: "m", time: "9:00", item: "Coblyn Larva + Gold Ore", zone: "Eastern Thanalan", area: "Burgandy Falls" },
+		{ type: "q", time: "13:00", item: "Grade 4 Carbonized Matter", zone: "Mor Dhona", area: "Saint Coinach's Find/Singing Shards" },
+		{ type: "q", time: "17:00", item: "Volcanic Rock Salt", zone: "Eastern La Noscea", area: "Raincatcher Gully" },
+		{ type: "q", time: "17:00", item: "Grade 4 Carbonized Matter", zone: "Mor Dhona", area: "Saint Coinach's Find/Singing Shards" },
+		{ type: "q", time: "21:00", item: "Astral Rock", zone: "Coerthas Central Highlands", area: "Skyfire Locks/Dragonhead" },
+		{ type: "q", time: "21:00", item: "Grade 4 Carbonized Matter", zone: "Mor Dhona", area: "Saint Coinach's Find/Singing Shards" },
+	],
+	botany: [
+		{ type: "h", time: "1:00", item: "Crawler Cocoons", zone: "Eastern Shroud", area: "Little Solace" },
+		{ type: "l", time: "1:00", item: "Grade 4 Carbonized Matter", zone: "Mor Dhona", area: "" },
+		{ type: "h", time: "5:00", item: "Trillium", zone: "Eastern Shroud", area: "" },
+		{ type: "h", time: "5:00", item: "Honey Lemon", zone: "Eastern La Noscea", area: "Gullperch Tower" },
+		{ type: "l", time: "5:00", item: "Grade 4 Carbonized Matter", zone: "Mor Dhona", area: "" },
+		{ type: "l", time: "9:00", item: "Spruce Log", zone: "Coerthas Central Highlands", area: "The Ogre's Belly" },
+		{ type: "h", time: "9:00", item: "Prickly Pineapple", zone: "Eastern La Noscea", area: "Costa Del Sol/Hidden Falls" },
+		{ type: "l", time: "9:00", item: "Grade 4 Carbonized Matter", zone: "Mor Dhona", area: "" },
+		{ type: "l", time: "13:00", item: "Vampire Plant", zone: "Coerthas Central Highlands", area: "Skyfire Locks/Dragonhead" },
+		{ type: "l", time: "17:00", item: "Thavnairian Mistletoe", zone: "Coerthas Central Highlands", area: "Stone Vigil/Whitebrim Front" },
+		{ type: "h", time: "21:00", item: "Kidragora & Trillium Bulb", zone: "Eastern Shroud", area: "The Honey Yard/The Bramble Patch" },
+	]
+};
